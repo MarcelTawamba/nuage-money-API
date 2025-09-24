29f8851f942e2e3ab8110d3000deb64b2eaf4d61
Marcel Tawamba <tawambamarcel@gmail.com>
2025-09-24 13:38:24 -0500

    feat: Implement direct package aliasing for Vite

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            '~': path.resolve(__dirname, 'node_modules')
+            'admin-lte': path.resolve(__dirname, 'node_modules/admin-lte')
         }
     },
 });

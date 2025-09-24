3320b5764e96aade0194b1ed7d2e5dc52f070906
Marcel Tawamba <tawambamarcel@gmail.com>
2025-09-24 14:18:13 -0500

    renaming vite alias

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import path from 'path';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '~': '/node_modules/'
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/sass/app.scss', 'resources/js/app.js'],

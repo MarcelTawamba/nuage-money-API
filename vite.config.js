0a7cd1184a1ba61cbc5113107ff1dfa8585b21b9
Marcel Tawamba <tawambamarcel@gmail.com>
2025-09-24 14:27:12 -0500

    add alias to resolve popperjs/core

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import path from 'path';
 export default defineConfig({
     resolve: {
         alias: {
-            '~': path.resolve(__dirname, 'node_modules/')
+            '~': path.resolve(__dirname, 'node_modules/'),
+            '@popperjs/core': path.resolve(__dirname, 'node_modules/@popperjs/core/dist/umd/popper.js')
         }
     },
     plugins: [

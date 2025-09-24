0ae81ae9e0e29f9a5f8c6e4c478959c1f879b075
Marcel Tawamba <tawambamarcel@gmail.com>
2025-09-24 12:45:11 -0500

    Fix vite build error again

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -10,16 +9,8 @@ export default defineConfig({
         }),
     ],
     resolve: {
-        alias: [
-            {
-                // this is required for the SCSS modules
-                find: /^~(.*)$/,
-                replacement: '$1',
-            },
-            {
-                find: 'admin-lte',
-                replacement: path.resolve(__dirname, 'node_modules/admin-lte'),
-            },
-        ],
+        alias: {
+            '~admin-lte': 'admin-lte',
+        },
     },
 });

2c5821c2e63f0a16489c0381ae9c64636da2f03d
Marcel Tawamba <tawambamarcel@gmail.com>
2025-10-27 14:14:48 -0500

    fix wallet balances retrieval error

--- app/Console/Commands/CheckStartButtonBalances.php
+++ app/Console/Commands/CheckStartButtonBalances.php
@@ -2,8 +2,8 @@
 
 namespace App\Console\Commands;
 
-use App\Classes\StartButtonAfricaPaymentHelper;
 use App\Notifications\LowBalanceWarning;
+use App\Services\StartButton\AfricaService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Notification;
 
@@ -26,12 +26,25 @@ class CheckStartButtonBalances extends Command
     /**
      * Execute the console command.
      */
-    public function handle(StartButtonAfricaPaymentHelper $startButtonAfricaPaymentHelper)
+    public function handle(AfricaService $startButtonAfricaService)
     {
         $thresholds = config('balance_thresholds.startbutton');
+        $walletBalanceResponse = $startButtonAfricaService->getWalletBalance();
+        $wallets = [];
+        if ($walletBalanceResponse['success']) {
+            $wallets = $walletBalanceResponse['data'];
+        }
+
+        // Create a map of wallets with currency as the key
+        $walletMap = [];
+        foreach ($wallets as $wallet) {
+            if (isset($wallet['currency'])) {
+                $walletMap[$wallet['currency']] = $wallet['availableBalance'];
+            }
+        }
 
         foreach ($thresholds as $currency => $threshold) {
-            $balance = $startButtonAfricaPaymentHelper->getWalletBalance($currency);
+            $balance = $walletMap[$currency] ?? 0;
 
             if ($balance < $threshold) {
                 Notification::route('mail', 'mtawamba@nuage.money')
